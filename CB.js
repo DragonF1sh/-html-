4a88cdc29d235bd9705fb6deee11c2d8bde2390f
Chris Boos <chrisboos2017@outlook.com>
2019-12-05 20:15:35 -0800

    ヾ(•ω•`)o

---
+++ CB.js
@@ -0,0 +1,28 @@
+/**
+var hi = ("<h1>Hewow, Fwend</h1>");
+
+for(var n10000 = 1; n10000 < 100; n10000++) {
+    document.write(hi);
+    document.write(n10000);
+}
+ */
+
+
+
+count2TenThousond(69);
+
+function count2TenThousond(number) {
+    for(var n10000 = 1; n10000 < 100; n10000+=2) {
+        document.write("<p>" + number + "</p>");
+    }
+}
+
+/**
+function expo(num1, num2) {
+    num1**num2
+}
+
+var solution = expo(10, 2)
+
+document.write(solution)
+*/
---
+++ new.js
@@ -0,0 +1,65 @@
+// creates an empty array (or "stack")
+var stack = [];
+
+// add to stack
+stack.push("pancake 1")
+document.write(stack);
+document.write("<br>");
+
+stack.push("pancake 2")
+document.write(stack);
+document.write("<br>");
+
+stack.push("pancake 3")
+document.write(stack);
+document.write("<br>");
+
+stack.push("pancake 4")
+document.write(stack);
+document.write("<br>");
+
+stack.push("pancake 5")
+document.write(stack);
+document.write("<br>");
+
+// take off stack
+stack.pop()
+document.write(stack);
+document.write("<br>");
+
+stack.pop()
+document.write(stack);
+document.write("<br>");
+
+//create the Q
+var queue = []
+
+// add to end of Q
+queue.push("person 1")
+document.write(queue);
+document.write("<br>");
+
+queue.push("person 2")
+document.write(queue);
+document.write("<br>");
+
+queue.push("person  3")
+document.write(queue);
+document.write("<br>");
+
+queue.push("person  4")
+document.write(queue);
+document.write("<br>");
+
+queue.push("person  5")
+document.write(queue);
+document.write("<br>");
+
+//take away form Q
+queue.shift()
+document.write(queue);
+document.write("<br>");
+
+queue.shift()
+document.write(queue);
+document.write("<br>");
